c20def38163e1352ec993ee6566fe8c45a949b40
arnab25 <arnabroy2597@gmail.com>
2020-09-28 17:04:50 +0530

    this is done by Arnab in Eclipse

---
+++ src/mainClass.java
@@ -0,0 +1,13 @@
+
+public class mainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Main Method");
+		System.out.println("a");
+		System.out.println("b");
+		System.out.println("c");
+		System.out.println("d");
+		
+	}
+
+}
